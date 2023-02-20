d08f2f9cd8371185477b9d18fee8dc10c8d80ef1
Chinta Sivanarayana <112633527+chintasiva@users.noreply.github.com>
2023-02-20 18:21:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,6 @@ const mongoose=require("mongoose")
 mongoose.set('strictQuery', false)
 require("dotenv").config()
 
-const connection=mongoose.connect(process.env.mongoURL)
+const connection=mongoose.connect(process.env.mongo_URL)
 
 module.exports={connection}
